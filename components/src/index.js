75b3b85c1e7a5b2b3dcd487a16303c60439abf59
Martins Bumanis <bumanis.martins@gmail.com>
2020-10-06 23:35:57 +0300

    Reusable components

--- components/src/index.js
+++ components/src/index.js
@@ -2,28 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
 return (
 <div className="ui container comments">
+<ApprovalCard>
+    <div>
+        <h4>Warning!</h4>
+    Are you sure you want to quit?
+    </div>
+</ApprovalCard>
+    
+    <ApprovalCard>
    <CommentDetail 
    author="Sam" 
    timeAgo="Today at 4:00AM" 
    avatarImage={faker.image.avatar()} 
    commentText={faker.lorem.sentence()}
    />
+   </ApprovalCard>
+
+   <ApprovalCard>
    <CommentDetail 
    author="Alex" 
    timeAgo="Today at 7:00PM" 
    avatarImage={faker.image.avatar()} 
    commentText={faker.lorem.sentence()}
    />
+   </ApprovalCard>
+
+   <ApprovalCard>
    <CommentDetail 
    author="Jane" 
    timeAgo="Yesterday at 5:00AM" 
    avatarImage={faker.image.avatar()} 
    commentText={faker.lorem.sentence()}
    />
+   </ApprovalCard>
 </div>
 
 );
