b6042c034a3d018f4324166b555bb0bdb562e76b
Martins Bumanis <bumanis.martins@gmail.com>
2020-10-06 19:30:13 +0300

    styling changes

--- components/src/index.js
+++ components/src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
 return (
 <div className="ui container comments">
-   <CommentDetail author="Sam" timeAgo="Today at 4:00AM" avatarImage={faker.image.avatar()} commentText={faker.lorem.sentence()}/>
-   <CommentDetail author="Alex" timeAgo="Today at 7:00PM" avatarImage={faker.image.avatar()} commentText={faker.lorem.sentence()}/>
-   <CommentDetail author="Jane" timeAgo="Yesterday at 5:00AM" avatarImage={faker.image.avatar()} commentText={faker.lorem.sentence()}/>
+   <CommentDetail 
+   author="Sam" 
+   timeAgo="Today at 4:00AM" 
+   avatarImage={faker.image.avatar()} 
+   commentText={faker.lorem.sentence()}
+   />
+   <CommentDetail 
+   author="Alex" 
+   timeAgo="Today at 7:00PM" 
+   avatarImage={faker.image.avatar()} 
+   commentText={faker.lorem.sentence()}
+   />
+   <CommentDetail 
+   author="Jane" 
+   timeAgo="Yesterday at 5:00AM" 
+   avatarImage={faker.image.avatar()} 
+   commentText={faker.lorem.sentence()}
+   />
 </div>
 
 );
